2092cf2b62e45dad72b4d9b8a8200bc939a4dac3
oh taejun <rewrite0w0@gmail.com>
2022-10-06 13:51:49 +0900

    개발 설정

--- pages/me.js
+++ pages/me.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>메인 작업하셈</div>;
+  return (
+    <div>
+      <div>메인 작업하셈</div>
+    </div>
+  );
 }
