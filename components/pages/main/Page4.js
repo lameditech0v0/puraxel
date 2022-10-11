1ad71318406411f84420e438039be003e61ceb21
dnwls7738 <cjsdnwls7738@gmail.com>
2022-10-11 14:26:13 +0900

    main

--- components/pages/main/Page4.js
+++ components/pages/main/Page4.js
@@ -22,8 +22,56 @@ function Page4() {
           speed={2000}
           navigation={true}
           spaceBetween={40}
-          slidesPerView={3}
+          slidesPerView={4.5}
         >
+          <SwiperSlide className="swiper__product">
+            <section>
+              <figure className="swiper__product__img">
+                <Image src={p1} />
+              </figure>
+              <article className="swiper__product__text">
+                <h1>퓨라셀 me</h1>
+                <p className="swiper__product__text__p1">
+                  전문적으로 사용하던 프락셔널 레이저의 효능을
+                  <br /> 직접 확인하실 수 있습니다. <br />
+                  하루 10분, 이제 집에서 관리하세요.
+                </p>
+                <p className="swiper__product__text__p2">1,800,000원</p>
+              </article>
+            </section>
+          </SwiperSlide>
+          <SwiperSlide className="swiper__product">
+            <section>
+              <figure className="swiper__product__img">
+                <Image src={p2} />
+              </figure>
+              <article className="swiper__product__text">
+                <h1>퓨라셀 me</h1>
+                <p className="swiper__product__text__p1">
+                  전문적으로 사용하던 프락셔널 레이저의 효능을
+                  <br /> 직접 확인하실 수 있습니다. <br />
+                  하루 10분, 이제 집에서 관리하세요.
+                </p>
+                <p className="swiper__product__text__p2">1,800,000원</p>
+              </article>
+            </section>
+          </SwiperSlide>
+          <SwiperSlide className="swiper__product">
+            <section>
+              <figure className="swiper__product__img">
+                <Image src={p3} />
+              </figure>
+              <article className="swiper__product__text">
+                <h1>퓨라셀 me</h1>
+                <p className="swiper__product__text__p1">
+                  전문적으로 사용하던 프락셔널 레이저의 효능을
+                  <br /> 직접 확인하실 수 있습니다. <br />
+                  하루 10분, 이제 집에서 관리하세요.
+                </p>
+                <p className="swiper__product__text__p2">1,800,000원</p>
+              </article>
+            </section>
+          </SwiperSlide>
           <SwiperSlide className="swiper__product">
             <section>
               <figure className="swiper__product__img">
