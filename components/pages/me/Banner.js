dc872727d3e177bde1b44531f88b0a977a87e7fc
dnwls7738 <cjsdnwls7738@gmail.com>
2022-10-11 15:37:56 +0900

    me

---
+++ components/pages/me/Banner.js
@@ -0,0 +1,10 @@
+function Banner() {
+  return (
+    <section id="me__page1">
+      <article className="me__header"></article>
+      <article></article>
+    </section>
+  );
+}
+
+export default Banner;
