26803d68c767cc80e62bb0ad640ddc32337cfcfd
dnwls7738 <cjsdnwls7738@gmail.com>
2022-10-17 15:58:07 +0900

    main 반응형

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,27 +3,63 @@ import {
   Box,
   Button,
   createTheme,
-  Divider,
   Drawer,
-  Fab,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
   ThemeProvider,
+  Typography,
 } from "@mui/material";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+
+const button = createTheme({
+  components: {
+    MuiListItemButton: {
+      styleOverrides: {
+        root: {
+          padding: 0,
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          padding: 0,
+          minWidth: 0,
+        },
+      },
+    },
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          "&:last-child": {
+            marginBottom: "7.29vw",
+          },
+        },
+      },
+    },
+  },
+});
 
 const theme = createTheme({
   components: {
     MuiTypography: {
       styleOverrides: {
         root: {
+          marginRight: "1.04vw",
           color: "#1A1A1A",
           fontFamily: "Pretendard",
           fontSize: "3.13vw",
           fontWeight: 700,
+        },
+      },
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        root: {
+          flex: "0 1 auto",
         },
       },
     },
@@ -43,8 +79,47 @@ const theme2 = createTheme({
         },
       },
     },
+    MuiListItemText: {
+      styleOverrides: {
+        root: {
+          flex: "0 1 auto",
+        },
+      },
+    },
   },
 });
+
+const menuHeader = createTheme({
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: "#B6BCC7",
+          fontFamily: "Pretendard",
+          fontSize: "1.56vw",
+          fontWeight: 400,
+        },
+      },
+    },
+  },
+});
+
+const snsButton = createTheme({
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          marginRight: "3.78vw",
+          color: "#1A1A1A",
+          fontFamily: "Pretendard",
+          fontSize: "3.13vw",
+          fontWeight: 700,
+        },
+      },
+    },
+  },
+});
+
 function Header() {
   // 햄버거 메뉴
   const [state, setState] = useState({
@@ -90,54 +165,111 @@ function Header() {
   const list = (anchor) => (
     <Box
       sx={{
-        width: "50vw",
+        width: "100vw",
         zIndex: 2,
+        paddingLeft: "3.13vw",
       }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <p>제품</p>
+      <ThemeProvider theme={menuHeader}>
+        <Typography variant="h1" gutterBottom>
+          제품
+        </Typography>
+      </ThemeProvider>
       <List>
         {[
           ["PURAXEL-Me", "퓨라셀 미"],
           ["PURAXEL", "퓨라셀"],
           ["PURAXEL-Hair", "퓨라셀 헤어"],
         ].map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ThemeProvider theme={theme}>
-                <ListItemText primary={text[0]} />
-              </ThemeProvider>
-              <ThemeProvider theme={theme2}>
-                <ListItemText primary={text[1]} />
-              </ThemeProvider>
-            </ListItemButton>
-          </ListItem>
+          <ThemeProvider key={text} theme={button}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ThemeProvider theme={theme}>
+                  <ListItemText primary={text[0]} />
+                </ThemeProvider>
+                <ThemeProvider theme={theme2}>
+                  <ListItemText primary={text[1]} />
+                </ThemeProvider>
+              </ListItemButton>
+            </ListItem>
+          </ThemeProvider>
         ))}
       </List>
-      <p>제품 구매</p>
+      <ThemeProvider theme={menuHeader}>
+        <Typography variant="h1" gutterBottom>
+          제품 구매
+        </Typography>
+      </ThemeProvider>
       <List>
-        {["Shop"].map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ThemeProvider theme={theme}>
-                <ListItemText primary={text} />
-              </ThemeProvider>
-            </ListItemButton>
-          </ListItem>
+        {[["Shop", "쇼핑몰"]].map((text) => (
+          <ThemeProvider key={text} theme={button}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ThemeProvider theme={theme}>
+                  <ListItemText primary={text[0]} />
+                </ThemeProvider>
+                <ThemeProvider theme={theme2}>
+                  <ListItemText primary={text[1]} />
+                </ThemeProvider>
+              </ListItemButton>
+            </ListItem>
+          </ThemeProvider>
         ))}
       </List>
-      <p>고객서비스</p>
+      <ThemeProvider theme={menuHeader}>
+        <Typography variant="h1" gutterBottom>
+          고객 서비스
+        </Typography>
+      </ThemeProvider>
       <List>
-        {["inquery", "newsletter"].map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ThemeProvider theme={theme}>
-                <ListItemText primary={text} />
-              </ThemeProvider>
-            </ListItemButton>
-          </ListItem>
+        {[
+          ["inquery", "제품 문의"],
+          ["newsletter", "뉴스레터"],
+        ].map((text) => (
+          <ThemeProvider key={text} theme={button}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ThemeProvider theme={theme}>
+                  <ListItemText primary={text[0]} />
+                </ThemeProvider>
+                <ThemeProvider theme={theme2}>
+                  <ListItemText primary={text[1]} />
+                </ThemeProvider>
+              </ListItemButton>
+            </ListItem>
+          </ThemeProvider>
+        ))}
+      </List>
+      <ThemeProvider theme={menuHeader}>
+        <Typography variant="h1" gutterBottom>
+          SNS 바로가기
+        </Typography>
+      </ThemeProvider>
+      <List>
+        {[
+          ["instagram", "footer/insta.png"],
+          ["facebook", "footer/facebook.png"],
+          ["youtube", "footer/youtube.png"],
+        ].map((text) => (
+          <ThemeProvider key={text} theme={button}>
+            <ul style={{ display: "inline-block" }}>
+              <li>
+                <ListItemButton>
+                  <ThemeProvider theme={snsButton}>
+                    <img
+                      src={text[1]}
+                      alt=""
+                      style={{ marginRight: "1.04vw" }}
+                    />
+                    <ListItemText primary={text[0]} />
+                  </ThemeProvider>
+                </ListItemButton>
+              </li>
+            </ul>
+          </ThemeProvider>
         ))}
       </List>
     </Box>
@@ -174,29 +306,10 @@ function Header() {
               </Link>
             </div>
           </div>
-
           {["right"].map((anchor) => (
             <div key={anchor}>
-              <Button onClick={toggleDrawer(anchor, true)}>
-                <svg
-                  width="32"
-                  height="32"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M7 9h18M7 16h18M7 23h18"
-                    stroke="#000000"
-                    stroke-width="1.5"
-                  />
-                </svg>
-              </Button>
-              <Drawer
-                anchor={anchor}
-                open={state[anchor]}
-                onClose={toggleDrawer(anchor, false)}
-              >
-                <Button onClick={toggleDrawer(anchor, false)}>
+              <ThemeProvider theme={button}>
+                <Button onClick={toggleDrawer(anchor, true)}>
                   <svg
                     width="32"
                     height="32"
@@ -204,12 +317,63 @@ function Header() {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      d="M7 8l18 16M7 24L25 8"
+                      d="M7 9h18M7 16h18M7 23h18"
                       stroke="#000000"
                       stroke-width="1.5"
                     />
                   </svg>
                 </Button>
+              </ThemeProvider>
+
+              <Drawer
+                anchor={anchor}
+                open={state[anchor]}
+                onClose={toggleDrawer(anchor, false)}
+              >
+                <ThemeProvider theme={button}>
+                  <div className="header__logo2">
+                    <div className="logo">
+                      <Link href="/" aria-label="logo">
+                        <svg
+                          width="8.85vw"
+                          height="4.04vw"
+                          viewBox="0 0 106 48"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M105.434 2.72131C106.185 1.96139 106.185 1.33083 105.434 0.570912C104.683 -0.189002 104.06 -0.189002 103.309 0.570912C98.5243 5.41219 93.7398 10.2535 88.9553 15.0947C89.663 15.8108 90.3729 16.5268 91.0805 17.2451C95.865 12.4039 100.65 7.56259 105.434 2.72131Z"
+                            fill="#1D1F36"
+                          />
+                          <path
+                            d="M77.3069 5.46243C70.2854 -1.64242 58.8993 -1.64242 51.8778 5.46243C48.7026 8.67532 45.5274 11.8882 42.3499 15.1034C43.0575 15.8194 43.7674 16.5355 44.475 17.2538C47.6503 14.0409 50.8255 10.828 54.003 7.61283C59.8512 1.69519 69.3335 1.69519 75.1818 7.61283C84.559 17.1014 93.9363 26.5899 103.311 36.0761C104.062 36.836 104.685 36.836 105.436 36.0761C106.187 35.3162 106.187 34.6857 105.436 33.9257L77.3069 5.46243Z"
+                            fill="#1D1F36"
+                          />
+                          <path
+                            d="M84.706 19.3819C81.5307 22.5948 78.3555 25.81 75.178 29.0229C69.3298 34.9405 59.8475 34.9405 53.9993 29.0229C46.2313 21.1627 38.4633 13.3026 30.6953 5.4424C19.3686 -6.01868 0 2.09785 0 18.3078V46.4801C0 47.5541 0.440559 47.9999 1.50201 47.9999C2.56346 47.9999 3.00402 47.5541 3.00402 46.4801V28.3784C9.29054 37.9501 22.6625 39.3013 30.6953 31.1733C33.8705 27.9604 37.0457 24.7475 40.2232 21.5323C39.5156 20.8162 38.8057 20.0979 38.0981 19.3819C34.9228 22.5948 31.7476 25.81 28.5701 29.0229C19.1358 38.5691 3.00402 31.8084 3.00402 18.3078C3.00402 4.80721 19.1358 -1.95349 28.5701 7.5928C36.3381 15.4529 44.1061 23.3131 51.8741 31.1733C58.8956 38.2781 70.2817 38.2781 77.3032 31.1733C80.4784 27.9604 83.6537 24.7475 86.8312 21.5323C86.1235 20.8162 85.4136 20.0979 84.706 19.3819Z"
+                            fill="#1D1F36"
+                          />
+                        </svg>
+                      </Link>
+                    </div>
+                    <div>
+                      <Button onClick={toggleDrawer(anchor, false)}>
+                        <svg
+                          width="32"
+                          height="32"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M7 8l18 16M7 24L25 8"
+                            stroke="#000000"
+                            stroke-width="1.5"
+                          />
+                        </svg>
+                      </Button>
+                    </div>
+                  </div>
+                </ThemeProvider>
                 {list(anchor)}
               </Drawer>
             </div>
