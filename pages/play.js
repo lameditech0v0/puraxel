8e0cb0d84e73839491a69bbf8c96a2735178796d
rewrite0w0 <rewrite0w0@gmail.com>
2022-10-17 11:38:15 +0900

    add: test mui

--- pages/play.js
+++ pages/play.js
@@ -1,3 +1,7 @@
+import * as React from "react";
+
+import { Button, Typography, Modal, Box } from "@mui/material";
+
 import ModeSelector from "components/modeSelector/ModeSelector";
 
 import { hairElement, fx5000Element } from "lib/mode";
@@ -8,8 +12,23 @@ const hair = [
   { title: "갈바닉", img: "url(modeSelector/hair_gal.webp)", num: "3" },
   { title: "쿨링", img: "url(modeSelector/hair_cooling.webp)", num: "4" },
 ];
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Play() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <section
       style={{ width: "100%", display: "flex", justifyContent: "center" }}
@@ -35,6 +54,25 @@ export default function Play() {
             image={x.img}
           />
         ))}
+      </div>
+
+      <div>
+        <Button onClick={handleOpen}>Open modal</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </div>
     </section>
   );
